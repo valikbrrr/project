f0ff36a487a1f56310417a405a77661917f3bb37
valik <valik.0404@mail.ru>
2023-07-12 02:52:26 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:vue/vue3-essential'
+	],
+	'overrides': [
+		{
+			'env': {
+				'node': true
+			},
+			'files': [
+				'.eslintrc.{js,cjs}'
+			],
+			'parserOptions': {
+				'sourceType': 'script'
+			}
+		}
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'plugins': [
+		'vue'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'space',
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
---
+++ js/script.js
@@ -0,0 +1,35 @@
+// alert('hi');
+
+// const result = confirm("you");
+
+// const answer = +prompt("vam est 18?", "");
+// console.log(answer + 5);
+
+
+
+
+
+// const answers = [];
+
+// answers[0] = prompt("как ваше имя?", "");
+// answers[1] = prompt("asxdcfvbnm?", "");
+// answers[2] = prompt("сколько вам лет?", "");
+
+// document.write(answers);
+
+
+
+
+const conclusion = "";
+
+conclusion = prompt("напиши своё имя", "");
+
+document.write(`привет, ${conclusion}`);
+
+
+
+
+
+// const user = "vano";
+
+// alert(`привет,  ${user}`);
