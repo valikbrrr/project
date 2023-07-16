73fd85e11e9f90a97f80d1cab7c2018d389f133b
valik <valik.0404@mail.ru>
2023-07-17 01:52:02 +0300

    commit after 1_lesson

--- js/script.js
+++ js/script.js
@@ -20,11 +20,11 @@
 
 
 
-const conclusion = "";
+// const conclusion = "";
 
-conclusion = prompt("напиши своё имя", "");
+// conclusion = prompt("напиши своё имя", "");
 
-document.write(`привет, ${conclusion}`);
+// document.write(`привет, ${conclusion}`);
 
 
 
@@ -33,3 +33,26 @@ document.write(`привет, ${conclusion}`);
 // const user = "vano";
 
 // alert(`привет,  ${user}`);
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt("Один из последних просмотренных фильмов", ""), 
+//     b = prompt("На сколько оцените его?", ""),
+//     c = prompt("Один из последних просмотренных фильмов", ""),
+//     d = prompt("На сколько оцените его?", "");
+
+//     personalMovieDB.movies[a] = b;
+//     personalMovieDB.movies[c] = d;
+
+
+    document.write(`${numberOfFilms}`);
+
+    console.log(numberOfFilms + 13);
