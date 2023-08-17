fa876042179d6559a4d4d29a42c2746b7a5360e1
valik <valik.0404@mail.ru>
2023-08-17 15:26:45 +0300

    sortStudentsByGroups, end udemy

--- js/script.js
+++ js/script.js
@@ -1,44 +1,23 @@
-const shoppingMallData = {
-    shops: [
-        {
-            width: 10,
-            length: 5
-        },
-        {
-            width: 15,
-            length: 7
-        },
-        {
-            width: 20,
-            length: 5
-        },
-        {
-            width: 8,
-            length: 10
-        }
-    ],
-    height: 5,
-    moneyPer1m3: 30,
-    budget: 50000
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    const a = [], b = [], c = [], rest = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i < 3) {
+            a.push(arr[i]);  
+        } else if (i < 6) {         
+            b.push(arr[i]);
+        } else if (i < 9) {
+            c.push(arr[i]);
+        } else {
+            rest.push(arr[i]);
+        }   
+    }
+
+    console.log(a, b, c, `Оставшиеся студенты: ${rest.length === 0 ? " - " : rest.join(", ") }`);
+    return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]
 }
 
-function isBudgetEnough(data) {
-    let square = 0;
-    let volume = 0;
-    str = "";
-
-    data.shops.forEach( shop => {
-    square += shop.width * shop.length;
-    });
-
-    volume += square * data.height;
-
-    data.budget - (volume * data.moneyPer1m3) >= 0 ? str = "Бюджета достаточно" : str = "Бюджета недостаточно";
-    console.log(str);
-    return str;
-}
-
-isBudgetEnough(shoppingMallData);
-
-
-
+sortStudentsByGroups(students);
